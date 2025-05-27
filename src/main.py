d85c2d97ada56c0e33fc19e9cb8c33b98c7ae093
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2025-05-26 17:46:13 -0700

    sample tool server added for beginning tool integration

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 from runner.gemini_runner import GeminiRunner
 from sessions.session_manager import SessionsManager
 from dotenv import load_dotenv
+from fastmcp import Client
+from samples.server import echo_mcp_server
+import asyncio
 
 load_dotenv()
+
+async def list_tools():
+    async with client:
+        result = await client.list_tools()
+        return result
+
+client = Client(echo_mcp_server)
+
+response = asyncio.run(list_tools())
+print("Sample mcp server response: {response}".format(response=response))
 
 session_manager = SessionsManager()
 instruction = "You are a general purpose agent to chat with the user"
---
+++ src/samples/server.py
@@ -0,0 +1,20 @@
+from mcp.server.fastmcp import FastMCP
+
+echo_mcp_server = FastMCP("Echo")
+
+@echo_mcp_server.resource("echo://{message}")
+def echo_resource(message: str) -> str:
+    """Echo a message as a resource"""
+    return f"Resource echo: {message}"
+
+
+@echo_mcp_server.tool()
+def echo_tool(message: str) -> str:
+    """Echo a message as a tool"""
+    return f"Tool echo: {message}"
+
+
+@echo_mcp_server.prompt()
+def echo_prompt(message: str) -> str:
+    """Create an echo prompt"""
+    return f"Please process this message: {message}"
