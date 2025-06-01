b7a26723b174992e7e98a1a57befbf84dd24480a
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2025-06-01 00:39:15 -0700

    dev testing script retained

---
+++ dev_testing/test.py
@@ -0,0 +1,18 @@
+from fastmcp import Client
+import asyncio
+
+async def test():
+    config = {
+        "mcpServers": {
+            "everything": {
+                "command": "npx",
+                "args": ["-y", "@modelcontextprotocol/server-everything"]
+            }
+        }
+    }
+
+    async with Client(config) as client:
+      tools = await client.list_tools()
+      print("TOOlS: {tools}".format(tools=tools))
+
+asyncio.run(test())
